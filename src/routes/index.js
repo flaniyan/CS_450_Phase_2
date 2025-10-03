176e46820fc811746a7f29ba80a230efc4b81947
flaniyan <flaniyan@purdue.edu>
2025-10-02 22:38:02 -0400

    move the tests under unit folder

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/hello", (_req, res) => res.json({ message: "hello world" }));
-
+router.use("/packages", require("./packages"));
 // ratings routes under /api/registry/models/:modelId/ratings...
 router.use("/", require("../services/rating"));
 
