13a7c0e0cc2f52a8ed510426ead84ab1a7d02240
tolawore1 <tolawore@purdue.edu>
2025-10-04 10:36:21 -0400

    infra: add Terraform scaffold (S3, DynamoDB, IAM); docs: team AWS login/setup

---
+++ infra/envs/dev/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "s3" {
+  source         = "../modules/s3"
+  artifacts_name = var.artifacts_bucket
+}
+
+module "ddb" {
+  source = "../modules/dynamodb"
+}
+
+module "iam" {
+  source            = "../modules/iam"
+  artifacts_bucket  = module.s3.artifacts_bucket
+  ddb_tables_arnmap = module.ddb.arn_map
+}
+
+output "artifacts_bucket" { value = module.s3.artifacts_bucket }
+
+
