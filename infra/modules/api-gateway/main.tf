1821619ac068088119d5b0ea5c1bcd206ed2c947
flaniyan <flaniyan@purdue.edu>
2025-11-06 16:39:17 -0500

    Add request validator to root GET method
    Introduces a basic request validator resource and associates it with the root GET method in the API Gateway configuration to enforce request parameter validation.

--- infra/modules/api-gateway/main.tf
+++ infra/modules/api-gateway/main.tf
@@ -25,14 +25,22 @@ resource "aws_api_gateway_rest_api" "main_api" {
   }
 }
 
+# Request Validator
+resource "aws_api_gateway_request_validator" "basic" {
+  name                        = "basic-validator"
+  rest_api_id                 = aws_api_gateway_rest_api.main_api.id
+  validate_request_parameters = true
+}
+
 # ===== ROOT LEVEL RESOURCES =====
 
 # GET / (root path)
 resource "aws_api_gateway_method" "root_get" {
-  rest_api_id   = aws_api_gateway_rest_api.main_api.id
-  resource_id   = aws_api_gateway_rest_api.main_api.root_resource_id
-  http_method   = "GET"
-  authorization = "NONE"
+  rest_api_id          = aws_api_gateway_rest_api.main_api.id
+  resource_id          = aws_api_gateway_rest_api.main_api.root_resource_id
+  http_method          = "GET"
+  authorization        = "NONE"
+  request_validator_id = aws_api_gateway_request_validator.basic.id
 }
 
 resource "aws_api_gateway_integration" "root_get" {
