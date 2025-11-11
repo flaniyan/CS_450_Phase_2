e92e0194ac546fffd1e32a1ace7cdc97f90413f2
Ethan Silverthorne <emsilver987@gmail.com>
2025-11-11 00:29:23 -0500

    add APi gateway to accept greedy path parameter

--- infra/modules/api-gateway/main.tf
+++ infra/modules/api-gateway/main.tf
@@ -272,7 +272,7 @@ resource "aws_api_gateway_resource" "artifact_byname" {
 resource "aws_api_gateway_resource" "artifact_byname_name" {
   rest_api_id = aws_api_gateway_rest_api.main_api.id
   parent_id   = aws_api_gateway_resource.artifact_byname.id
-  path_part   = "{name}"
+  path_part   = "{name+}"
 }
 
 # /artifact/byRegEx
