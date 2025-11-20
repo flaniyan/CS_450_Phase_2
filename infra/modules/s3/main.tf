228c5f01f48fea9d4c25e4f99fbede21de94c58f
Ali Afrose <mdaliafrose@gmail.com>
2025-11-20 00:01:42 -0500

    use terraform to form access point that matches the codebase

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -10,6 +10,27 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   }
 }
 
+# S3 Access Point for secure access to the bucket
+resource "aws_s3_access_point" "main" {
+  name   = "cs450-s3"
+  bucket = aws_s3_bucket.artifacts.id
+
+  public_access_block_configuration {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets  = true
+  }
+
+  lifecycle {
+    # Prevent destruction of access point if bucket is being destroyed
+    prevent_destroy = false
+  }
+}
+
 output "artifacts_bucket" { value = aws_s3_bucket.artifacts.id }
+output "access_point_arn" { 
+  value = aws_s3_access_point.main.arn 
+}
 
 
