6f5c8647ff0d738eb2cb2bd3ce975a531de7448a
Ali Afrose <mdaliafrose@gmail.com>
2025-11-24 18:48:39 -0500

    updated encryption configuration to use KMS with the actual key and terraform will manage the encryption config

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -7,8 +7,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.artifacts.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = "arn:aws:kms:us-east-1:838693051036:key/ffc50d00-4db1-4676-a63a-c7c1e286abfc"
     }
+    bucket_key_enabled = false
   }
 }
 
